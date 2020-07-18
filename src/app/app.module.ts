63cf33e466dc034668426cfd7fd84f9fccac0012
Hien Pham <hienpham@b13technology.com>
2020-07-18 23:54:09 +0700

    Add recurring event choosercomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RecurringEventChooserComponent } from './recurring-event-chooser/recurring-event-chooser.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecurringEventChooserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/recurring-event-chooser/recurring-event-chooser.component.ts
@@ -0,0 +1,141 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NgbCalendar, NgbDate, NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
+import {Frequency, Options, RRule} from 'rrule';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+
+export function toNativeDate(ngbDate: NgbDate): Date {
+  return new Date(Date.UTC(ngbDate.year, ngbDate.month - 1, ngbDate.day));
+}
+
+@Component({
+  selector: 'app-recurring-event-chooser',
+  templateUrl: './recurring-event-chooser.component.html',
+  styleUrls: ['./recurring-event-chooser.component.scss']
+})
+export class RecurringEventChooserComponent implements OnInit, OnDestroy {
+  Frequency = Frequency;
+  recurringForm: FormGroup;
+  hoveredDate: NgbDate | null = null;
+  dates: Date[] = [];
+
+  private today: NgbDate;
+  private weekdayMap = [
+    RRule.MO,
+    RRule.TU,
+    RRule.WE,
+    RRule.TH,
+    RRule.FR,
+    RRule.SA,
+    RRule.SU
+  ];
+  private destroy$ = new Subject();
+
+  get f(): any {
+    return this.recurringForm.controls;
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private calendar: NgbCalendar,
+    public formatter: NgbDateParserFormatter
+  ) { }
+
+  ngOnInit(): void {
+    this.today = this.calendar.getToday();
+    this.initRecurringForm();
+    this.subscribeToFormValue();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onDateSelection(date: NgbDate): void {
+    if (!this.f.startDate.value && !this.f.endDate.value) {
+      this.f.startDate.setValue(date);
+    } else if (this.f.startDate.value && !this.f.endDate.value && date && date.after(this.f.startDate.value)) {
+      this.f.endDate.setValue(date);
+    } else {
+      this.f.endDate.setValue(null);
+      this.f.startDate.setValue(date);
+    }
+  }
+
+  isHovered(date: NgbDate): boolean {
+    return this.f.startDate.value &&
+      !this.f.endDate.value &&
+      this.hoveredDate &&
+      date.after(this.f.startDate.value) &&
+      date.before(this.hoveredDate);
+  }
+
+  isInside(date: NgbDate): boolean {
+    return this.f.endDate.value && date.after(this.f.startDate.value) && date.before(this.f.endDate.value);
+  }
+
+  isRange(date: NgbDate): boolean {
+    return date.equals(this.f.startDate.value) ||
+      (this.f.endDate.value && date.equals(this.f.endDate.value)) ||
+      this.isInside(date) || this.isHovered(date);
+  }
+
+  validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+    const parsed = this.formatter.parse(input);
+    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
+  }
+
+  setStartDate(value: string): void {
+    this.f.startDate.setValue(this.validateInput(this.f.startDate.value, value));
+  }
+
+  setEndDate(value: string): void {
+    this.f.endDate.setValue(this.validateInput(this.f.endDate.value, value));
+  }
+
+  private initRecurringForm(): void {
+    this.recurringForm = this.fb.group({
+      startDate: [this.today, Validators.required],
+      endDate: [this.calendar.getNext(this.today, 'd', 7), Validators.required],
+      frequency: [Frequency.DAILY],
+      onWeekday: this.fb.array(
+        [false, false, false, false, false, false, false].map(val => this.fb.control(val))
+      ),
+      onMonthday: [this.today]
+    });
+  }
+
+  private subscribeToFormValue(): void {
+    this.recurringForm.valueChanges.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe((value) => {
+      const options: Partial<Options> = {
+        freq: value.frequency || Frequency.DAILY,
+        dtstart: toNativeDate(value.startDate || this.today),
+        until: toNativeDate(value.endDate || this.today),
+        byweekday: value.frequency === Frequency.WEEKLY ?
+          this.getWeekday(value.onWeekday) : null,
+        bymonthday: value.frequency === Frequency.MONTHLY ?
+          (value.onMonthday && value.onMonthday.day || this.today.day) : null
+      };
+      console.log('options', options);
+      const rule = new RRule(options);
+      this.dates = rule.all();
+    });
+
+    this.recurringForm.patchValue({
+      startDate: this.today,
+      endDate: this.calendar.getNext(this.today, 'd', 7),
+      frequency: Frequency.DAILY
+    });
+  }
+
+  private getWeekday(byWeekday: boolean[]): any {
+    const result = byWeekday
+      .map((v, i) => v && this.weekdayMap[i] || null)
+      .filter(v => !!v);
+    return result.length ? result : null;
+  }
+}
